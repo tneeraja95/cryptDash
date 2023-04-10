0e3d1ee18cafc60be804c4db27665150a15f9e4b
neeraja <tneeraja95@gmail.com>
2023-04-10 19:13:29 +0530

    Feat: Fetching the currency chart data

---
+++ src/components/CurrencyChart.jsx
@@ -0,0 +1,12 @@
+
+
+function CurrencyChart({prices}) {
+    console.log(prices);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CurrencyChart
--- src/components/CurrencyDetail.jsx
+++ src/components/CurrencyDetail.jsx
@@ -1,13 +1,26 @@
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import CurrencyChart from "./CurrencyChart";
 
 function CurrencyDetail({}) {
-    const {id} = useParams();
-    console.log( id);
-  return (
-    <div>
-      
-    </div>
-  )
+  const { id } = useParams();
+  const [coinChartData, setCoinChartData] = useState({});
+  useEffect(() => {
+    (async function () {
+      let apiresponse = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?x_cg_api_key=CG-piQbqGwkhbXruXiJCUXL71j9&vs_currency=usd&days=1`);
+      let response = await apiresponse.json();
+      setCoinChartData( response);
+   //   console.log(response);
+    })();
+  }, []);
+
+  if(coinChartData){
+    let prices = coinChartData.prices.map(price => ( [new Date(price[0]), price[1] ]));
+  //  console.log(coinChartData, prices);
+  }
+  return <>
+    {coinChartData && <CurrencyChart prices={coinChartData.prices.map(price => ( [new Date(price[0]), price[1] ]))}/>}
+  </>;
 }
 
-export default CurrencyDetail
+export default CurrencyDetail;
