3b5cd1d0bd3a79bad8124760451b7f9c7e1bf07d
neeraja <tneeraja95@gmail.com>
2023-04-13 19:13:29 +0530

    Feat: Chart in progress using highcharts

--- src/components/CurrencyChart.jsx
+++ src/components/CurrencyChart.jsx
@@ -1,12 +1,162 @@
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+import highchartsAccessibility from "highcharts/modules/accessibility";
 
+highchartsAccessibility(Highcharts);
 
-function CurrencyChart({prices}) {
-    console.log(prices);
+function CurrencyChart({ coinChartData }) {
+  console.log("inside currreny chart");
+  let setPlotLines = true;
+
+  function handleRangeChange(thisObj) {
+    console.log(thisObj);
+    let plotPointValue = thisObj.series[0].points[0].y;
+    let yAxis = thisObj.yAxis[0];
+    console.log(plotPointValue);
+    yAxis.update({
+      plotLines: [
+        {
+          color: "#000",
+          width: 1,
+          dashStyle: "dot",
+          value: plotPointValue,
+        },
+      ],
+    });
+  }
+
+  Highcharts.StockChart("container", {
+    yAxis: {
+      crosshair: true,
+    },
+
+    xAxis: {
+      type: "datetime",
+      crosshair: true,
+    },
+
+    time: {
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    },
+
+    series: [
+      {
+        name: "Prices",
+        data: coinChartData.prices,
+        zones: [
+          {
+            //     value: plotLineValue,
+            color: "rgb(234, 57, 67)", //red
+          },
+          { color: "rgb(22, 199, 132)" },
+        ],
+        fillOpacity: 1,
+      },
+      // {name: "Market cap", data: coinChartData.market_caps},
+    ],
+
+    chart: {
+      events: {
+        click() {
+          console.log("redraw is triggered");
+          let plotPointValue = thisObj.series[0].data.find(
+            (el) => el !== undefined
+          ).y;
+          handleRangeChange(this);
+        },
+        render() {
+          console.log("render is triggered", setPlotLines);
+          if (setPlotLines) {
+            setPlotLines = false;
+            handleRangeChange(this);
+          }
+        },
+      },
+    },
+    tooltip: {
+      shared: true,
+      useHTML: true,
+      formatter: function () {
+        //  console.log(this);
+        let dateTime = new Date(this.x);
+        let date = dateTime.toLocaleDateString();
+        let time = dateTime.toLocaleTimeString();
+        return date + "      " + time + "\nPrice " + this.y; // + " \nVolume " + this.points[1].y;
+      },
+    },
+    legend: {
+      enabled: false,
+    },
+
+    rangeSelector: {
+      allButtonsEnabled: true,
+      selected: 0,
+      buttons: [
+        {
+          type: "day",
+          count: 1,
+          text: "1d",
+          title: "View 1 day",    
+           events : {
+            click: function(){console.log(this); setPlotLines=true}
+          }
+     
+        },
+        {
+          type: "day",
+          count: 7,
+          text: "7d",
+          title: "View 7 days",
+          events : {
+            click: function(){console.log(this); setPlotLines=true}
+          }
+        },
+        {
+          type: "month",
+          count: 1,
+          text: "1m",
+          title: "View 1 months",
+          events : {
+            click: function(){console.log(this); setPlotLines=true}
+          }
+     
+        },
+        {
+          type: "month",
+          count: 3,
+          text: "3m",
+          title: "View 3 months",
+          events : {
+            click: function(){console.log(this); setPlotLines=true}
+          }
+     
+        },
+      ],
+    },
+
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            legend: {
+              layout: "horizontal",
+              align: "center",
+              verticalAlign: "bottom",
+            },
+          },
+        },
+      ],
+    },
+  });
+
   return (
-    <div>
-      
+    <div style={{ margin: "20px" }}>
+      <div id="container" />
     </div>
-  )
+  );
 }
 
-export default CurrencyChart
+export default CurrencyChart;
--- src/components/CurrencyDetail.jsx
+++ src/components/CurrencyDetail.jsx
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CurrencyChart from "./CurrencyChart";
+import parseChartData from "../utils/parseChartData";
 
 function CurrencyDetail({}) {
   const { id } = useParams();
-  const [coinChartData, setCoinChartData] = useState({});
+  const [coinChartData, setCoinChartData] = useState([]);
   useEffect(() => {
-    (async function () {
-      let apiresponse = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?x_cg_api_key=CG-piQbqGwkhbXruXiJCUXL71j9&vs_currency=usd&days=1`);
-      let response = await apiresponse.json();
-      setCoinChartData( response);
-   //   console.log(response);
-    })();
+    async function fetchData() {
+      let apiresponse = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?x_cg_api_key=CG-piQbqGwkhbXruXiJCUXL71j9&vs_currency=usd&days=89`);   
+      let coinChartData = await apiresponse.json();
+    //  console.log(coinChartData);
+      setCoinChartData(coinChartData);
+    };
+    fetchData();
   }, []);
 
-  if(coinChartData){
-    let prices = coinChartData.prices.map(price => ( [new Date(price[0]), price[1] ]));
-  //  console.log(coinChartData, prices);
-  }
+//   if(coinChartData){
+//     let prices = coinChartData.prices.map(price => ( [new Date(price[0]), price[1] ]));
+//   //  console.log(coinChartData, prices);
+//   }
+
+//console.log(coinChartData );
   return <>
-    {coinChartData && <CurrencyChart prices={coinChartData.prices.map(price => ( [new Date(price[0]), price[1] ]))}/>}
+  <div id="container"></div>
+    {Object.keys(coinChartData).length > 0 && <CurrencyChart coinChartData = {coinChartData}/>}
   </>;
 }
 
